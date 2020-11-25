6ecb293181fc9e0e77b77fc2185c94829438bad5
Massil Taguemout <massitaguemout@gmail.com>
2020-11-26 00:52:41 +0100

    fix xsd & code/pseudocode for business logic

--- project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/impl/TrainServiceImpl.java
+++ project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/impl/TrainServiceImpl.java
@@ -8,6 +8,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 @ApplicationScoped
 @ManagedBean
@@ -35,5 +36,36 @@ public class TrainServiceImpl implements TrainService {
         /**
          * Autres règles
          */
+    }
+
+    public void calculateDelay(LiveInfo liveInfo) {
+        /**
+         * ICI : Récupérer les informations théoriques du train depuis la base de données,
+         *  Comparer avec les informations reçues :
+         *      - train = jpa.find(trainId)
+         *      - List<Desserte> dessertes = train.getDesserte()
+         *      - planifiedDeparture = dessertes.get(lastGareId)...
+         *      - planifiedArrival = dessertes.get(nextGareId)...
+         */
+        LocalDateTime planifiedDeparture = null;
+        LocalDateTime planifiedArrival = null;
+        LocalDateTime now = liveInfo.getTimestamp();
+
+        int diff = liveInfo.getPercentage() - shouldBeAtPercent(planifiedDeparture, planifiedArrival, now);
+        if (Math.abs(diff) > 10) {
+            if (diff < 0) {
+                // retard() de diff% = pourcentage de la duration entre planifiedDep et planifiedArr
+            } else {
+                // avance()
+            }
+        }
+
+    }
+
+    public int shouldBeAtPercent(LocalDateTime departure, LocalDateTime arrival, LocalDateTime live) {
+        Duration totalDuration = Duration.between(departure, arrival);
+        Duration betweenDuration = Duration.between(departure, live);
+
+        return (int)((double)betweenDuration.toSeconds()*100/(double)totalDuration.toSeconds());
     }
 }
